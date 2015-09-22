b74e24994c795b3eee993013c1503d4a60559330
Sam Joseph <tansaku@gmail.com>
2015-09-22 17:09:45 +0100

    double quotes to support newlines

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -5,13 +5,13 @@ readme_updated = false # git comparison across branches ...
 vacuous_tests = false # would have to mutate tests out and check code coverage stats (not foolproof) - could also check for stubbing method under test ...
 sensitive_data = false # could be done with regex
 
-LATEST_RUBY = '* The Gemfile can lock the project to a particular version of Ruby. '\
-              ' In general you should prefer the latest, currently 2.2.3\n'
+LATEST_RUBY = "* The Gemfile can lock the project to a particular version of Ruby. "\
+              " In general you should prefer the latest, currently 2.2.3\n"
 
-RUBY_TWILIO = '* Including all the gems you use in your Gemfile is an important courtesy'\
-              ' to other developers and yourself in the future, so that all the project'\
-              ' dependencies can be pulled in whenever the project is checked out on a'\
-              ' new machine, e.g. `twilio-ruby` gem\n'
+RUBY_TWILIO = "* Including all the gems you use in your Gemfile is an important courtesy"\
+              " to other developers and yourself in the future, so that all the project"\
+              " dependencies can be pulled in whenever the project is checked out on a"\
+              " new machine, e.g. `twilio-ruby` gem\n"
 
 File.foreach('Gemfile').with_index do |line, line_num|
   # require 'byebug' ; byebug
