083b07113626cb63739c9039e070b8884d6123ce
Sam Joseph <tansaku@gmail.com>
2015-09-23 17:05:31 +0100

    updates name of twilio-ruby gem to be correct

--- lib/scanner.rb
+++ lib/scanner.rb
@@ -2,7 +2,7 @@
 
 
 latest_ruby = false # working on first pass
-ruby_twilio_included = false # working on first pass - more general checks for other needed gems? (check for build failure)
+twilio_ruby_included = false # working on first pass - more general checks for other needed gems? (check for build failure)
 rspec_can_run = false # would have to run rspec, supress output and check the result
 readme_updated = not(`git diff master README.md`.empty?) # git comparison across branches ... `git diff master README.md`
 vacuous_tests = false # would have to mutate tests out and check code coverage stats (not foolproof) - could also check for stubbing method under test ...
@@ -25,9 +25,9 @@ README_UPDATE = "* Please do update your README following the [contribution note
 File.foreach('Gemfile').with_index do |line, line_num|
   # require 'byebug' ; byebug
   latest_ruby ||=  !(line =~ /ruby '2\.2\.3'/).nil?
-  ruby_twilio_included ||= !(line =~ /require 'ruby-twilio'/).nil?  # we need to check that ruby-twilio is in fact used somewhere else as well
+  twilio_ruby_included ||= !(line =~ /gem 'twilio-ruby'/).nil?  # we need to check that ruby-twilio is in fact used somewhere else as well
 end
 
 puts LATEST_RUBY unless latest_ruby
-puts RUBY_TWILIO unless ruby_twilio_included
+puts RUBY_TWILIO unless twilio_ruby_included
 puts README_UPDATE unless readme_updated
